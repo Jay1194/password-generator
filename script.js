b58d6e8bf243d5023ac746d926b9751bfb59667c
github_Jay1194 <j.thomastt1@gmail.com>
2022-06-15 21:50:02 -0600

    defining variables

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-// Assignment code here
 
-
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+// Naming Variables
+
+var clickGeneratePassword
+var confirmLowercase
+var confirmUppercase
+var confirmNumeric
+var specialCharacters
+
+// Defining Variables
+
+confirmLowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+confirmUppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+
+
+confirmNumeric = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+specialCharacters = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
 
 // Write password to the #password input
 function writePassword() {
@@ -15,3 +30,10 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Start Function
+
+
+// User Input
+
+
